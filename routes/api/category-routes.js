9056be846e8895531c544f06f71d8f6a1edcd6e2
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-08-18 20:48:08 +1000

    added code tofind category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,11 +3,10 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-// router.get('/', (req, res) => {
 router.get('/', async (req, res) => {
     try {
       const categories = await Category.findAll({
-        include: [Product], // Include associated Products
+        include: [Product],
       });
       res.status(200).json(categories);
     } catch (err) {
@@ -15,9 +14,32 @@ router.get('/', async (req, res) => {
     }
   });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // Find one category by its `id` value and include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+
+    const categoryData = await Category.findOne({
+      where: {
+        id: categoryId 
+      },
+      include: [
+        { model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+        }
+      ]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+
+    res.json(categoryData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
