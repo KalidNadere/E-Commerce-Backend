c8fc642417b749ecd3a7578ddf1dd548bd1cf24d
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-08-19 16:27:41 +1000

    added code to update product

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,8 +46,15 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+
+// Create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(201).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
